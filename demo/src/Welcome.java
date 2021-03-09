ee39eee3097a06ca9d3fcc1f7579a557c7b239ac
syndicate017 <geniuses17@gmail.com>
2021-03-10 02:22:55 +0700

    Add Java exercise - demo

---
+++ demo/src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+//        Display massage Welcome to Java! on the console
+        System.out.println("Welcome to Java!");
+    }
+}
