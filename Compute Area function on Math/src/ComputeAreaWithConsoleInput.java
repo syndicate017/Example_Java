20a5d93cef01ad27242c32ae4b094efd6a311f14
syndicate017 <geniuses17@gmail.com>
2021-03-15 07:38:44 +0700

    Update Java exercise - Compute Area function on Math with Console input and Constant variable

--- Compute Area function on Math/src/ComputeAreaWithConsoleInput.java
+++ Compute Area function on Math/src/ComputeAreaWithConsoleInput.java
@@ -2,21 +2,23 @@ import java.util.*; /*Scanner is in the java.util package*/
 
 public class ComputeAreaWithConsoleInput {
     public static void main(String[] args) {
-        /*Declare a constant*/
-        final double PI = 3.14159;
-
         /*Create a Scanner object*/
         Scanner input = new Scanner(System.in);
+
+        /*Declare a constant*/
+        final double PI = 3.14159;
 
         /*Prompt the user to enter a radius*/
         System.out.print("Enter a number for radius: ");
         double radius = input.nextDouble();
 
         /*Compute area*/
-        double area = radius * radius * PI;
-
-        /*Display results*/
-        System.out.println("The area for the circle of radius " +
-        radius + " is " + area);
+        if (radius >= 0) {
+            double area = radius * radius * PI;
+            System.out.println("The area for the circle of radius " +
+                    radius + " is " + area);
+        } else {
+            System.out.println("Negative input");
+        }
     }
 }
